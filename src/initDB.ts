02927c84eb663aa56a0939ab3dab652d63977fd2
ebaze-tech <ebaze.technologies@gmail.com>
2026-02-16 12:44:26 +0100

    Fixed types

--- src/initDB.ts
+++ src/initDB.ts
@@ -1,23 +1,15 @@
-import { Client } from 'pg'
+import { pool } from './config/db.config'
 import fs from 'fs'
 import path from 'path'
 
 export const initializeDb = async () => {
-  const client = new Client({
-    connectionString: process.env.DATABASE_URL
-  })
-
   try {
-    await client.connect()
-    // Path to your SQL file
-    const sqlFile = fs.readFileSync(path.join(__dirname, 'init.sql'), 'utf8')
+    const sqlFile = fs.readFileSync(path.join(__dirname, 'schema.sql'), 'utf8')
 
-    // Execute the SQL queries
-    await client.query(sqlFile)
+    await pool.query(sqlFile)
     console.log('Database initialized successfully')
   } catch (err) {
     console.error('Initialization failed:', err)
-  } finally {
-    await client.end()
+    throw err
   }
 }
